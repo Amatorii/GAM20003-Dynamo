5da8d5e5be7d678b6ee78245a446c639172156bf
SWIN\103998768 <103998768@student.swin.edu.au>
2023-09-27 12:02:31 +1000

    Added: UI Scripts

---
+++ Assets/Folders/Electra's Assets/Scripts/UI Scripts/ButtonScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour
+{
+
+    void Start()
+    {
+        Debug.Log("Start");
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    public void Pressed() 
+    {
+        Debug.Log("Pressed");
+    }
+}
