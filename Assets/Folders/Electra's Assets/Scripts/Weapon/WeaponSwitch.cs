2cc17954f45a27266e1c2f80d686a9859f3306f1
ElectraUehara <103998768@student.swin.edu.au>
2023-10-06 14:56:16 +1100

    Updated: weapon_master can now swap weapons using the scroll and numbers

--- Assets/Folders/Electra's Assets/Scripts/Weapon/WeaponSwitch.cs
+++ Assets/Folders/Electra's Assets/Scripts/Weapon/WeaponSwitch.cs
@@ -10,20 +10,13 @@ namespace Electra.weapon
 
         public List<GameObject> weaponList;
         public int weaponIndex = 0;
-
-        // Start is called before the first frame update
-        void Start()
-        {
-            //weaponList = FindObjectsByType<>
-        }
-    
-        // Update is called once per frame
+      
         void Update()
         {
             if (Input.mouseScrollDelta.y > 0)
             {
                 weaponIndex++;
-                if (weaponIndex == 3)
+                if (weaponIndex == weaponList.Count)
                     weaponIndex = 0;
                 SwapWeapon(weaponList[weaponIndex]);
             }
@@ -31,16 +24,15 @@ namespace Electra.weapon
             {
                 weaponIndex--;
                 if (weaponIndex == -1)
-                    weaponIndex = 2;
+                    weaponIndex = weaponList.Count;
                 SwapWeapon(weaponList[weaponIndex]);
             }
 
-            //if (Input.GetKeyDown(KeyCode.Alpha1))
-            //    SwapWeapon(shotgun);
-            //if (Input.GetKeyDown(KeyCode.Alpha2))
-            //    SwapWeapon(rocketLauncher);
-            //if (Input.GetKeyDown(KeyCode.Alpha3))
-            //    SwapWeapon(thirdGun);
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+                SwapWeapon(weaponList[0]);
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+                SwapWeapon(weaponList[1]);
+            
         }
         public void SwapWeapon(GameObject w)
         {
--- Assets/Folders/Kate's Assets/Scripts/weapons/weapon_master.cs
+++ Assets/Folders/Kate's Assets/Scripts/weapons/weapon_master.cs
@@ -8,7 +8,8 @@ public class weapon_master : MonoBehaviour
     public GameObject hitDecal;
 
     private GameObject currentWeapon;
-    public GameObject[] weapons;
+    public List<GameObject> weapons;
+    public int weaponIndex;
 
     [SerializeField] private string activeWeapon;
     // debug
@@ -18,11 +19,15 @@ public class weapon_master : MonoBehaviour
 
     weapon_state wpnShotgun;
     weapon_state wpnFireball;
+    List<weapon_state> weaponFunction;
+
 
     void Awake()
     {
         wpnShotgun = new wpn_shotgun(transform, hitDecal);
         wpnFireball = new wpn_fireball(transform, fireball);
+
+        weaponFunction = new List<weapon_state> { wpnFireball, wpnShotgun };
 
         wpnActive = wpnFireball;
         activeWeapon = wpnActive.name;
@@ -31,21 +36,49 @@ public class weapon_master : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetButtonDown("Fire3"))
+        if (Input.mouseScrollDelta.y > 0)
         {
-            if (wpnActive == wpnFireball)
-            {
-                wpnActive = wpnShotgun;
-                UpdateViewmodel(weapons[1]);
-            }
-            else
-            {
-                wpnActive = wpnFireball;
-                UpdateViewmodel(weapons[0]);
-            }
-
-            activeWeapon = wpnActive.name;
+            weaponIndex++;
+            if (weaponIndex == weapons.Count)
+                weaponIndex = 0;
+            wpnActive = weaponFunction[weaponIndex];
+            UpdateViewmodel(weapons[weaponIndex]);
         }
+        if (Input.mouseScrollDelta.y < 0)
+        {
+            weaponIndex--;
+            if (weaponIndex == -1)
+                weaponIndex = weapons.Count - 1;
+            wpnActive = weaponFunction[weaponIndex];
+            UpdateViewmodel(weapons[weaponIndex]);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            UpdateViewmodel(weapons[0]);
+            wpnActive = wpnFireball;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            UpdateViewmodel(weapons[1]);
+            wpnActive = wpnShotgun;
+        }
+
+        //if (Input.GetButtonDown("Fire3"))
+        //{
+        //    if (wpnActive == wpnFireball)
+        //    {
+        //        wpnActive = wpnShotgun;
+        //        UpdateViewmodel(weapons[1]);
+        //    }
+        //    else
+        //    {
+        //        wpnActive = wpnFireball;
+        //        UpdateViewmodel(weapons[0]);
+        //    }
+        //
+        //    activeWeapon = wpnActive.name;
+        //}
         // this is a dev feature just so we can try these 2 out
 
         else if (Input.GetButtonDown("Fire1"))
@@ -55,7 +88,7 @@ public class weapon_master : MonoBehaviour
     void UpdateViewmodel(GameObject newWeapon)
     {
         currentWeapon.SetActive(false);
-        newWeapon.SetActive(true);
         currentWeapon = newWeapon;
+        currentWeapon.SetActive(true);
     }
 }
