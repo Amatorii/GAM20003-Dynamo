12f60436191d2fd3a237f0bd3b6c449f1895b052
ElectraUehara <103998768@student.swin.edu.au>
2023-10-17 16:48:08 +1100

    Added: Pause to playtest scene

--- Assets/Folders/Electra's Assets/Scripts/UI Scripts/Pause.cs
+++ Assets/Folders/Electra's Assets/Scripts/UI Scripts/Pause.cs
@@ -21,7 +21,7 @@ public class Pause : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.P))
+        if (Input.GetKeyDown(KeyCode.P) | Input.GetKeyDown(KeyCode.Escape))
         {
             if (!paused) 
                 PauseGame();
